fab806d0e5b2a65fe70cadf4b7067a6cbf3ef3f3
alampert <a.lampert@posteo.de>
2018-02-23 08:59:03 +0100

    server.log

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const hbs = require('hbs')
+const fs = require('fs')
 
 // Creting the app
 const app = express()
@@ -8,12 +9,33 @@ hbs.registerPartials(__dirname + '/views/partials/')
 
 // Setting  key/value pair
 app.set('view engine', 'hbs')
+
+// Some kind of middleware: a logger
+app.use((req, res, next) => {
+  const now = new Date().toString()
+  const log = `${now}: ${req.method} ${req.url}`
+  console.log(log)
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to  append to server.log')
+    }
+  })
+  next()
+})
+
+// stop execution by not calling next()
+app.use((req, res, next) => {
+  res.render('maintenance.hbs')
+})
+
+// serving static content
 app.use(express.static(__dirname + '/public'))
 
+// Helper
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear()
 })
-
+// Helper mit Argumenten
 hbs.registerHelper('screamIt', (text) => {
   return text.toUpperCase()
 })
